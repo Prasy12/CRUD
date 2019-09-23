85a1fd1a32a07a49abf7526342c076d4bd2ddd33
Prasanna <prasanna.venkatesh@psiog.com>
2019-09-23 14:43:59 +0530

    Update adminaccess.js

--- adminaccess.js
+++ adminaccess.js
@@ -135,9 +135,9 @@ function doShowAll() {
     
        var key =0;
        
-       var list = "<tr><th>S.No</th><th>FullName</th><th>Email</th><th>Phone Number</th><th>Educational Qualifications</th><th>Fathers Name</th><th>Mothers Name</th><th>Nationality</th><th>Gender</th><th>Address</th><th>Zipcode</th><th>Date of Birth</th><th>Status</th><th>Delete</th></tr>\n";
-           key =  JSON.parse(localStorage.getItem("appname"));;
-           key1 = JSON.parse(localStorage.getItem("mail"));;
+       var list = "<tr><th>S.No</th><th>FullName</th><th>Email</th><th>Phone Number</th><th>Educational Qualifications</th><th>Fathers Name</th><th>Mothers Name</th><th>Nationality</th><th>Gender</th><th>Address</th><th>Zipcode</th><th>Date of Birth</th><th>Status</th></tr>\n";
+           key =  JSON.parse(localStorage.getItem("appname"));
+           key1 = JSON.parse(localStorage.getItem("mail"));
            key2= JSON.parse(localStorage.getItem('phno'));
            key3 = JSON.parse(localStorage.getItem('EQ'));
            key4 = JSON.parse(localStorage.getItem('fname'));
@@ -147,16 +147,23 @@ function doShowAll() {
            key8 = JSON.parse(localStorage.getItem('address'));
            key9 = JSON.parse(localStorage.getItem('zip'));
            key10 = JSON.parse(localStorage.getItem('dob'));
+           if(key==null)
+           {
+               alert("No applications found!")
+               window.location.assign("index.html");
+           }
+           else
+           {
            for(i=0;i<key.length;i++)
            { str= "status" + i;
            var sno=i+1; 
            list += "<tr><td>"+ sno +"</td>\n<td>" + key[i] + "</td>\n<td>"
            + key1[i] +"</td>\n<td>"+key2[i]+"</td>\n<td>"+key3[i]+"</td>\n<td>"+key4[i]+"</td>\n<td>"+
            key5[i]+"</td>\n<td>"+key6[i]+"</td>\n<td>"+key7[i]+"</td>\n<td>"+key8[i]+"</td>\n<td>"+key9[i]+ 
-           "</td>\n<td>"+key10[i]+"</td><td><select value='1' id=" + str + "><option value=''>select</option><option value='Approved'>Approved</option><option value='Rejected'>Rejected</option></select></td><td><input type=submit onclick=deletee(this) value='Delete Selected' id=btn-delete></td></tr>\n";
+           "</td>\n<td>"+key10[i]+"</td><td><select value='1' id=" + str + "><option value=''>select</option><option value='Approved'>Approved</option><option value='Rejected'>Rejected</option></select></td></tr>\n";
            }
            
-           
+        }
        document.getElementById('list').innerHTML = list; 
        
 
@@ -166,7 +173,7 @@ function doShowAll() {
 var str;
 function Save(){
     
-        var list2 = "<tr><th>S.No</th><th>FullName</th><th>Email</th><th>Phone Number</th><th>Educational Qualifications</th><th>Fathers Name</th><th>Mothers Name</th><th>Nationality</th><th>Gender</th><th>Address</th><th>Zipcode</th><th>Date of Birth</th><th>Status</th></tr>\n";
+        var list2 = "<tr><th>S.No</th><th>FullName</th><th>Email</th><th>Phone Number</th><th>Educational Qualifications</th><th>Fathers Name</th><th>Mothers Name</th><th>Nationality</th><th>Gender</th><th>Address</th><th>Zipcode</th><th>Date of Birth</th><th>Status</th><th>Delete</th></tr>\n";
         key =  JSON.parse(localStorage.getItem("appname"));;
         key1 = JSON.parse(localStorage.getItem("mail"));;
         key2= JSON.parse(localStorage.getItem('phno'));
@@ -199,7 +206,7 @@ function Save(){
        list2 += "<tr><td>" +sno+"</td>\n<td>" + key[i] + "</td>\n<td>"
        + key1[i] +"</td>\n<td>"+key2[i]+"</td>\n<td>"+key3[i]+"</td>\n<td>"+key4[i]+"</td>\n<td>"
        +key5[i]+"</td>\n<td>"+key6[i]+"</td>\n<td>"+key7[i]+"</td>\n<td>"+key8[i]+"</td>\n<td>"+key9[i]+
-       "</td>\n<td>"+key10[i]+"</td>\n<td>"+key11[i].str+"</td></tr>\n";
+       "</td>\n<td>"+key10[i]+"</td>\n<td>"+key11[i].str+"</td><td><input type=submit onclick=deletee(this) value='Delete ' id=btn-delete></td></tr>\n";
        }
       
        document.getElementById('list').innerHTML =list2 ;  
