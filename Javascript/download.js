a788e4797e044ff49fb46476958dc0e294ed645a
Prasanna <prasanna.venkatesh@psiog.com>
2019-12-04 11:34:00 +0530

    Update download.js

--- Javascript/download.js
+++ Javascript/download.js
@@ -17,9 +17,8 @@ function downloadApplication()
         key8 = JSON.parse(localStorage.getItem('address'));
         key10 = JSON.parse(localStorage.getItem('dob'));
       
-    
-    alert("Congrats "+key[test]+"! Your application has been approved!");
-          
+   
+   
         var a="Name:";
         var b="Fathers' Name:";
         var c="Mothers' Name:";
@@ -38,7 +37,6 @@ function downloadApplication()
     else if(validate==null)
     {
         alert("Oops!! Your application is still being processed...");
-        alert("Redirecting you back to your application!")
         window.location.assign("../Sources/trackapplication.html");
     }
     else
